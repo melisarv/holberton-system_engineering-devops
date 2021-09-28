ddc33cd5c043b767e0188031ad43b62703dde398
melisarv <melisarv@gmail.com>
2021-09-28 15:37:42 -0500

    add task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""queries the Reddit API and return a list the titles of all hot articles"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None, count=0):
+    """return a list the titles"""
+    url = 'https://www.reddit.com'
+    query = '/r/' + subreddit + '/hot.json'
+    res = requests.get(url+query,
+                       params={"count": count, "after": after},
+                       headers={"User-agent": "your bot 0.1"})
+    if res.status_code != 200:
+        return None
+
+    h_list = hot_list + [i.get("data").get("title")
+                         for i in res.json()
+                         .get("data")
+                         .get("children")]
+
+    result = res.json()
+    if not result.get("data").get("after"):
+        return h_list
+
+    return recurse(subreddit, h_list, result.get("data").get("after"),
+                   result.get("data").get("count"))
