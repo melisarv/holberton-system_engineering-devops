ef5a89b8906e1bc304c919a0825f967d18152242
melisarv <melisarv@gmail.com>
2021-09-29 15:51:44 -0500

    update advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # set up ssh config file for connect to a server without typing a password
-file_line { 'id file stuff':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
-}
-
 file_line { 'psw no':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
+}
+
+file_line { 'id file stuff':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
